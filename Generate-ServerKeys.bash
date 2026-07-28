2d9aac8c5ba13a9e921066fb6c4d3e23902b16fc
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-07-27 22:14:01 -0300

    Removendo scripts bash

--- Generate-ServerKeys.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Generating private and public keys
-wg genkey | tee server.key | wg pubkey > server.pub
--- Get-DefaultInterfaceIPv4.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-command_output="$(ip route list table main default)"
-IFS=' '
-read -ra ADDR <<< "$command_output"
-echo "${ADDR[4]}"
