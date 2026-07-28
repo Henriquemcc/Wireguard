a09d1811b165333c5b1b16046a056f719b4995f8
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-07-27 22:42:10 -0300

    Revert "Separando as classes em diferentes arquivos"
    This reverts commit a1eaca4df4644090d35932268c557e82a1ea1bda.

--- Wireguard.ps1
+++ Wireguard.ps1
@@ -1,5 +1,133 @@
-﻿Import-Module -Name .\Server.ps1 -Global
-Import-Module -Name .\Client.ps1 -Global
+﻿class IpAddressInfo {
+    [System.Net.IPAddress]$IpAddress
+    [System.UInt16]$Prefix
+
+    IpAddressInfo([System.Net.IPAddress]$IpAddress, [System.UInt16]$Prefix) {
+        $this.IpAddress = $IpAddress
+        $this.Prefix = $Prefix
+    }
+
+    IpAddressInfo([System.String]$IpAddress, [System.UInt16]$Prefix) {
+        $this.IpAddress = [System.Net.IPAddress]::Parse($IpAddress)
+        $this.Prefix = $Prefix
+    }
+
+    [System.String]ToString() {
+        return "$($this.IpAddress)/$($this.Prefix)"
+    }
+
+    [IpAddressInfo] Clone() {
+        $clonedIpAddress = [System.Net.IPAddress]::Parse($this.IpAddress.ToString())
+        return [IpAddressInfo]::new($clonedIpAddress, $this.Prefix)
+    }
+}
+
+class Server {
+    [System.Collections.Generic.List[IpAddressInfo]]$Address
+    [System.String]$Endpoint
+    [System.UInt16]$Port
+    [System.String]$OutputInterfaceName
+    [System.String]$PrivateKey
+
+    Server([System.Collections.Generic.List[IpAddressInfo]]$Address, [System.String]$Endpoint, [System.UInt16]$Port, [System.String]$OutputInterfaceName, [System.String]$PrivateKey) {
+        $this.Address = $Address
+        $this.Endpoint = $Endpoint
+        $this.Port = $Port
+        $this.OutputInterfaceName = $OutputInterfaceName
+        $this.PrivateKey = $PrivateKey
+    }
+
+    Server([System.String]$Endpoint, [System.String]$OutputInterfaceName) {
+        $this.Address = @([IpAddressInfo]::new("10.100.0.1", 24), [IpAddressInfo]::new("fd08:4711::1", 64))
+        $this.Endpoint = $Endpoint
+        $this.Port = 51820
+        $this.OutputInterfaceName = $OutputInterfaceName
+        $this.PrivateKey = GeneratePrivateKey
+    }
+
+    Server([System.String]$Endpoint, [System.UInt16]$Port, [System.String]$OutputInterfaceName) {
+        $this.Address = @([IpAddressInfo]::new("10.100.0.1", 24), [IpAddressInfo]::new("fd08:4711::1", 64))
+        $this.Endpoint = $Endpoint
+        $this.Port = $Port
+        $this.OutputInterfaceName = $OutputInterfaceName
+        $this.PrivateKey = GeneratePrivateKey
+    }
+
+    [Server]Clone() {
+        $clonedAddress = [System.Collections.ArrayList]::new()
+        foreach ($address in $this.Address) {
+            [void]$clonedAddress.Add($address.Clone())
+        }
+        return [Server]::new($clonedAddress, $this.Endpoint.Clone(), $this.Port, $this.OutputInterfaceName.Clone(), $this.PrivateKey.Clone())
+    }
+
+    [System.String] GetPublicKey() {
+        return GeneratePublicKey -PrivateKey ($this.PrivateKey)
+    }
+
+}
+
+class Client {
+    [System.String]$Name
+    [System.Collections.Generic.List[IpAddressInfo]]$Address
+    [System.String]$PrivateKey
+    [System.String]$PresharedKey
+    [System.Collections.Generic.List[System.Net.IPAddress]]$DnsServers
+    [System.Collections.Generic.List[IpAddressInfo]]$AllowedIps
+
+    Client([System.String]$Name) {
+
+        $this.PrivateKey = GeneratePrivateKey
+        $this.PresharedKey = GeneratePresharedKey
+        $this.DnsServers = @([System.Net.IPAddress]::Parse("9.9.9.9"), [System.Net.IPAddress]::Parse("149.112.112.112"),[System.Net.IPAddress]::Parse("2620:fe::fe"),[System.Net.IPAddress]::Parse("2620:fe::9"),[System.Net.IPAddress]::Parse("1.1.1.2"),[System.Net.IPAddress]::Parse("1.0.0.2"),[System.Net.IPAddress]::Parse("2606:4700:4700::1112"),[System.Net.IPAddress]::Parse("2606:4700:4700::1002"),[System.Net.IPAddress]::Parse("8.8.8.8"),[System.Net.IPAddress]::Parse("8.8.4.4"),[System.Net.IPAddress]::Parse("2001:4860:4860::8888"),[System.Net.IPAddress]::Parse("2001:4860:4860::8844"))
+        $this.AllowedIps = @([IpAddressInfo]::new("0.0.0.0", 0), [IpAddressInfo]::new("::", 0))
+        $this.Name = $Name
+        $this.Address = $null
+    }
+
+    Client([System.String]$Name, [System.Collections.Generic.List[IpAddressInfo]]$Address) {
+
+        $this.PrivateKey = GeneratePrivateKey
+        $this.PresharedKey = GeneratePresharedKey
+        $this.DnsServers = @([System.Net.IPAddress]::Parse("9.9.9.9"), [System.Net.IPAddress]::Parse("149.112.112.112"),[System.Net.IPAddress]::Parse("2620:fe::fe"),[System.Net.IPAddress]::Parse("2620:fe::9"),[System.Net.IPAddress]::Parse("1.1.1.2"),[System.Net.IPAddress]::Parse("1.0.0.2"),[System.Net.IPAddress]::Parse("2606:4700:4700::1112"),[System.Net.IPAddress]::Parse("2606:4700:4700::1002"),[System.Net.IPAddress]::Parse("8.8.8.8"),[System.Net.IPAddress]::Parse("8.8.4.4"),[System.Net.IPAddress]::Parse("2001:4860:4860::8888"),[System.Net.IPAddress]::Parse("2001:4860:4860::8844"))
+        $this.AllowedIps = @([IpAddressInfo]::new("0.0.0.0", 0), [IpAddressInfo]::new("::", 0))
+        $this.Name = $Name
+        $this.Address = $Address
+    }
+
+    Client(
+        [System.String]$Name, [System.Collections.Generic.List[IpAddressInfo]]$Address, [System.String]$PrivateKey, [System.String]$PresharedKey, [System.Collections.Generic.List[System.Net.IPAddress]]$DnsServers, [System.Collections.Generic.List[IpAddressInfo]]$AllowedIps) {
+        $this.Name = $Name
+        $this.Address = $Address
+        $this.PrivateKey = $PrivateKey
+        $this.PresharedKey = $PresharedKey
+        $this.DnsServers = $DnsServers
+        $this.AllowedIps = $AllowedIps
+    }
+
+    [Client]Clone() {
+        $clonedAddress = [System.Collections.Generic.List[IpAddressInfo]]::new()
+        foreach ($address in $this.Address) {
+            [void]$clonedAddress.Add($address.Clone())
+        }
+
+        $clonedDnsServers = [System.Collections.Generic.List[System.Net.IPAddress]]::new()
+        foreach ($dnsServer in $this.DnsServers) {
+            [void]$clonedDnsServers.Add([System.Net.IPAddress]::Parse($dnsServer.ToString()))
+        }
+
+        $clonedAllowedIps = [System.Collections.Generic.List[IpAddressInfo]]::new()
+        foreach ($allowedIp in $this.AllowedIps) {
+            [void]$clonedAllowedIps.Add($allowedIp.Clone())
+        }
+
+        return [Client]::new($this.Name.Clone(), $clonedAddress, $this.PrivateKey.Clone(), $this.PresharedKey.Clone(), $clonedDnsServers, $clonedAllowedIps)
+    }
+
+    [System.String] GetPublicKey() {
+        return GeneratePublicKey -PrivateKey ($this.PrivateKey)
+    }
+}
 
 function GeneratePrivateKey() {
     return $(wg genkey)
